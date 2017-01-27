8b0bbf7295f382c76975de5f9cafa422fd8b8d7a
Dave Marchant <dwfmarchant@gmail.com>
2017-01-27 10:30:24 -0800

    Added MakeGrid

--- Scratch/__init__.py
+++ Scratch/__init__.py
@@ -3,3 +3,4 @@ from .LineBuilder import LineBuilder
 from .ReadData import readMag, readTopo
 from .MagData import MagData
 from .DCIP import appRes, writeDCIP
+from .MakeGrid import makeGrid, maskGrid
