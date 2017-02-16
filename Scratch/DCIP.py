c7e6867bca9c307033d43ddd9fd8139fcf9420f9
Dave Marchant <dwfmarchant@gmail.com>
2017-02-16 14:03:45 -0800

    Added readDCIP and readOut functions.

--- Scratch/DCIP.py
+++ Scratch/DCIP.py
@@ -1,4 +1,6 @@
+
 import numpy as np
+import pandas as pd
 
 def calcK(Ax, Ay, Bx, By, Mx, My, Nx, Ny):
     dx_am = Ax-Mx
@@ -26,7 +28,8 @@ def appRes(Ax, Ay, Bx, By, Mx, My, Nx, Ny, Vp, In=1.):
     return rho
 
 def writeDCIP(fname, data):
-    formatters = 4*[lambda x: '{:.2f}'.format(x)] + 2*[lambda x: '{:.4e}'.format(x)]
+
+    formatters = 4*['{:.2f}'.format] + 2*['{:.4e}'.format]
 
     grp = data.groupby(['Ax', 'Ay', 'Bx', 'By'])
     with open(fname, 'w') as f:
@@ -40,3 +43,113 @@ def writeDCIP(fname, data):
                             header=False, index=False)
             f.write('\n')
 
+def readDCIP(fname, hasZ=True, hasUncert=True):
+
+    if hasZ:
+        columns = ['Ax', 'Ay', 'Az',
+                   'Bx', 'By', 'Bz',
+                   'Mx', 'My', 'Mz',
+                   'Nx', 'Ny', 'Nz',
+                   'Vp']
+        colInds = {'Ax':0, 'Ay':1, 'Az':2,
+                   'Bx':3, 'By':4, 'Bz':5,
+                   'Mx':0, 'My':1, 'Mz':2,
+                   'Nx':3, 'Ny':4, 'Nz':5,
+                   'Vp':6, 'Un':7}
+    else:
+        columns = ['Ax', 'Ay',
+                   'Bx', 'By',
+                   'Mx', 'My',
+                   'Nx', 'Ny',
+                   'Vp']
+        colInds = {'Ax':0, 'Ay':1,
+                   'Bx':2, 'By':3,
+                   'Mx':0, 'My':1,
+                   'Nx':2, 'Ny':3,
+                   'Vp':4, 'Un':5}
+
+    if hasUncert: 
+        columns.append('Un')
+
+    survey = pd.DataFrame(columns=columns)
+
+    nLines = sum(1 for line in open(fname, 'r'))+1
+
+    with open(fname, 'r') as f:
+        for i in range(nLines):
+            line = f.readline()            
+            
+            # Remove comments
+            try:
+                commentInd = line.index('!')
+                line = line[:commentInd]
+            except Exception: 
+                pass
+
+            if line.strip().startswith('IPTYPE'): 
+                continue
+            
+            line = line.split()
+
+            # Skip empty lines
+            if len(line) == 0: 
+                continue
+
+            nRx = int(line[-1])    
+
+            Ax = float(line[colInds['Ax']])*np.ones(nRx)
+            Ay = float(line[colInds['Ay']])*np.ones(nRx)
+            Bx = float(line[colInds['Bx']])*np.ones(nRx)
+            By = float(line[colInds['By']])*np.ones(nRx)
+            Mx = np.zeros(nRx)
+            My = np.zeros(nRx)            
+            Nx = np.zeros(nRx)
+            Ny = np.zeros(nRx)
+            Vp = np.zeros(nRx)
+            if hasZ:
+                Az = float(line[colInds['Az']])*np.ones(nRx)
+                Bz = float(line[colInds['Bz']])*np.ones(nRx)
+                Mz = np.zeros(nRx)
+                Nz = np.zeros(nRx)
+            if hasUncert: 
+                Un = np.zeros(nRx)
+
+            for k in range(nRx):
+                line = f.readline().split()
+
+                Mx[k] = float(line[colInds['Mx']])
+                My[k] = float(line[colInds['My']])
+                Nx[k] = float(line[colInds['Nx']])
+                Ny[k] = float(line[colInds['Ny']])
+                Vp[k] = float(line[colInds['Vp']])
+                if hasZ:
+                    Mz[k] = float(line[colInds['Mz']])
+                    Nz[k] = float(line[colInds['Nz']])
+                if hasUncert: 
+                    Un[k] = float(line[colInds['Un']])
+
+                d = {'Ax':Ax, 'Ay':Ay,
+                     'Bx':Bx, 'By':By,
+                     'Mx':Mx, 'My':My,
+                     'Nx':Nx, 'Ny':Ny,
+                     'Vp':Vp}
+                if hasZ:
+                    d['Az'] = Az
+                    d['Bz'] = Bz
+                    d['Mz'] = Mz
+                    d['Nz'] = Nz
+                if hasUncert: 
+                    d['Un'] = Un
+
+            lineDF = pd.DataFrame(d)
+            survey = survey.append(lineDF, ignore_index=True)
+
+    return survey
+
+def readOut(fname='DC_octree_inv.out', onlyLast=True):
+    cols = ["beta", "iter", "misfit", "phi_d", "phi_m", "phi", "normg", "grel"]
+    out = pd.read_csv(fname, delim_whitespace=True, skiprows=1, names=cols)
+    if onlyLast:
+        out = out.loc[[0]].append(out.drop_duplicates('beta', keep='last'))
+        out = out.reset_index(drop=True)
+    return out
--- Scratch/__init__.py
+++ Scratch/__init__.py
@@ -2,5 +2,5 @@
 from .LineBuilder import LineBuilder
 from .ReadData import readMag, readTopo
 from .MagData import MagData
-from .DCIP import calcK, appRes, writeDCIP
+from .DCIP import calcK, appRes, writeDCIP, readDCIP, readOut
 from .MakeGrid import makeGrid, maskGrid
