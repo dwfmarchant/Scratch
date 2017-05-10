73427d22fbda37106ce16fbc6001575c7cbaa263
Dave Marchant <dwfmarchant@gmail.com>
2017-05-10 08:19:47 -0700

    Remove code now in InvTools.

--- Scratch/__init__.py
+++ Scratch/__init__.py
@@ -3,6 +3,4 @@ from .LineBuilder import LineBuilder
 from .ReadData import readMag, readTopo
 from .MagData import MagData
 from .DCIP import calcK, appRes, writeDCIP, readDCIP, readOut
-from .MakeGrid import makeGrid, maskGrid
 from .DCIP2D  import removePadding2D
-from .GenUtils import rotatePoints
